4fc9568c00f29729102e46ecdbbbba87f969eebc
Vincent Louie L. Abad <vabad@reddragonfarm.com>
2022-07-19 09:28:48 +0800

    Update View Index of all transactions

--- app/Http/Resources/TransactionIndex.php
+++ app/Http/Resources/TransactionIndex.php
@@ -29,7 +29,7 @@ class TransactionIndex extends JsonResource
                    $is_latest = 1;
                }
                 
-            return [
+            $transactions_details = [
                 "id"=> $this->id,
                 "is_latest_transaction"=> $is_latest,
                 "users_id"=>  $this->users_id,
@@ -53,9 +53,34 @@ class TransactionIndex extends JsonResource
                 "users"=> $this->users,
                 "po_details"=> $this->po_details
             ];
-
            }
+           
+           $transactions_details = [
+            "id"=> $this->id,
+            "is_latest_transaction"=> 1,
+            "users_id"=>  $this->users_id,
+            "request_id"=> $this->request_id,
+            "supplier_id"=> $this->supplier_id,
+            "document_id"=> $this->document_id,
+            "transaction_id"=> $this->transaction_id,
+            "document_type"=> $this->document_type,
+            "payment_type"=> $this->payment_type,
+            "supplier"=> $this->supplier,
+            "remarks"=> $this->remarks,
+            "date_requested"=> $this->date_requested,
+            "company"=> $this->company,
+            "department"=> $this->department,
+            "location"=> $this->location,
+            "document_no"=> $this->document_no,
+            "document_amount"=> $this->document_amount,
+            "referrence_no"=> $this->referrence_no,
+            "referrence_amount"=> $this->referrence_amount,
+            "status"=> $this->status,
+            "users"=> $this->users,
+            "po_details"=> $this->po_details
+        ];
        }
-    
+       
+       return $transactions_details;
     }
 }
