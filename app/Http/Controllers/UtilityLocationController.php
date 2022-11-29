ffdd053f9e8835d0d4be6a7b20aefc5929374148
Vincent Louie  Abad <vincent.abad@reddragonfarm.com>
2022-11-29 17:08:20 +0800

    Adjust Utility Location dropdown

--- app/Http/Controllers/UtilityLocationController.php
+++ app/Http/Controllers/UtilityLocationController.php
@@ -12,36 +12,44 @@ class UtilityLocationController extends Controller
 {
   public function index(Request $request)
   {
+
     $status =  $request['status'];
     $rows =  (empty($request['rows']))?10:(int)$request['rows'];
     $search =  $request['search'];
     $paginate = (isset($request['paginate']))? $request['paginate']:$paginate = 1;
     $category = (isset($request['category']))? $request['category']:NULL;
-    
-     $utility_locations= UtilityLocation::withTrashed()
-    ->with('credit_cards.utility_categories')
-    ->where(function ($query) use ($status){
-      ($status==true)?$query->whereNull('deleted_at'):$query->whereNotNull('deleted_at');
-    })
-    ->where('location', 'like', '%'.$search.'%')
-    ->latest('updated_at');
-    
-    if ($paginate == 1){
-      $utility_locations = $utility_locations
-      ->paginate($rows);
-    }else if ($paginate == 0){
-      $utility_locations = $utility_locations
-      ->when(isset($category), function($query) use ($category){
-        $query->whereHas('credit_cards.utility_categories', function ($query) use($category){
-          $query->where('id','<>',$category);
-        });
-      })
-      ->without('credit_cards')
-      ->get(['id','location as name']);
-      if(count($utility_locations)==true){
-          $utility_locations = array("utility_locations"=>$utility_locations);;
-      }
-    }
+
+   $utility_locations = DB::table('utility_locations')
+   ->when($status, function ($query){
+      $query->whereNull('deleted_at');
+   }, function ($query){
+      $query->whereNotNull('deleted_at');
+   })
+   ->where('location','like','%'.$search.'%');
+
+   if($paginate == 0){
+    $utility_Locations_ids = DB::table('credit_card_utility_locations')
+      ->selectRaw("utility_location_id")
+      ->leftJoin('credit_card_utility_categories','credit_card_utility_locations.credit_card_id','=','credit_card_utility_categories.credit_card_id')
+    ->where("credit_card_utility_categories.utility_category_id","=",$category)->pluck('utility_location_id');
+
+     $utility_locations = DB::table('utility_locations')
+     ->when($status, function ($query){
+        $query->whereNull('utility_locations.deleted_at');
+     }, function ($query){
+        $query->whereNotNull('utility_locations.deleted_at');
+     })
+     ->where('location','like','%'.$search.'%')
+     ->leftJoin('credit_card_utility_locations','utility_locations.id','=','credit_card_utility_locations.utility_location_id')
+     ->leftJoin('credit_card_utility_categories','credit_card_utility_locations.credit_card_id','=','credit_card_utility_categories.credit_card_id')
+     ->leftJoin('utility_categories','credit_card_utility_categories.utility_category_id','=','utility_categories.id')
+     ->whereNotIn('utility_locations.id',$utility_Locations_ids)
+     ->groupBy('utility_locations.id','utility_locations.location')
+     ->get(['utility_locations.id','utility_locations.location as name']);
+   }else{
+    $utility_locations = $utility_locations->latest()
+    ->paginate($rows);
+   }
     
     if(count($utility_locations)==true){
       return $this->resultResponse('fetch','Utility Location',$utility_locations);
